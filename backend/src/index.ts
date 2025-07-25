a807f25f7384d465c129544cb8d053105882fef2
vindex <vindex@fastmail.com>
2025-07-26 01:31:02 +0200

    integration tests for API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,10 @@
-import express from 'express';
+import app from './server';
 import dotenv from 'dotenv';
-import {ping} from './api/ping';
-import {createUserApi, getAllUsersApi} from "./api/users";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get('/', ping);
-
-const userRouter = express.Router();
-
-userRouter.post('/users', createUserApi);
-
-userRouter.get('/users', getAllUsersApi);
-
-app.use('/api/v1', userRouter);
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {ping} from './api/ping';
+import {createUserApi, getAllUsersApi} from "./api/users";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', ping);
+
+const userRouter = express.Router();
+
+userRouter.post('/users', createUserApi);
+
+userRouter.get('/users', getAllUsersApi);
+
+app.use('/api/v1', userRouter);
+
+export default app;
