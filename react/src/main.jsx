086f9c9cec20c56c2f72b350818dfd1a13da8791
bobasafe <safelgpreciado@gmail.com>
2025-07-25 10:00:45 -0700

    tailwind

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1 className="text-4xl bold underline">Hello World</h1>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
