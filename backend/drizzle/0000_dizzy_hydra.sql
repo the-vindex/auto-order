012ed82f6f734e5c23ca910746c78c3d725d1697
vindex <vindex@fastmail.com>
2025-07-26 21:05:14 +0200

    restarted drizzle migrations

--- backend/drizzle/0000_dizzy_hydra.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "users" (
-	"user_id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"full_name" text,
-	"email" text NOT NULL,
-	"password" text NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
--- backend/drizzle/0001_massive_bastion.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_email_unique";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "email";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "password";
