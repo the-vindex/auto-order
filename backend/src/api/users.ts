d6a50d1b2a93f8866ff14181a54a6e68b7c36465
wbartholomay <willbartholomay@gmail.com>
2025-07-26 07:13:21 -0400

    more auth stuff

--- backend/src/api/users.ts
+++ backend/src/api/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { createUser, getAllUsers } from "../db/queries/user_queries";
+import { createUser, getAllUsers, getUserByEmail } from "../db/queries/user_queries";
+import { verifyPassword } from "../auth/auth";
 
 export async function createUserApi(req: express.Request, res: express.Response) {
 	try {
@@ -9,6 +10,8 @@ export async function createUserApi(req: express.Request, res: express.Response)
 
 		const { name, email, password } = req.body;
 
+		//prob shouldnt be logging emails/passwords
+		console.log(`reating user with name ${name} email ${email} password ${password}`)
 		const newUser = await createUser(name, email, password);
 		res.status(201).json(newUser);
 	} catch (error) {
@@ -25,4 +28,31 @@ export async function getAllUsersApi(req: express.Request, res: express.Response
 		console.error('Error fetching users:', error);
 		res.status(500).send('Internal Server Error');
 	}
+}
+
+export async function loginUserApi(req: express.Request, res: express.Response) {
+	try {
+		const { email, password } = req.body;
+
+		console.log(`Logging in user ${email}`)
+
+		const user = await getUserByEmail(email);
+
+		console.log('Verifying password...')
+		const doPasswordsMatch = await verifyPassword(password, user.password);
+		console.log(`Password verification result: ${doPasswordsMatch}`)
+
+		if (!doPasswordsMatch) {
+			res.status(401).send('Invalid username/password.');
+			return;
+		}
+
+		res.status(200).send()
+
+	} catch (error) {
+		console.error('Error logging in user:', error);
+		res.status(500).send('Internal Server Error');
+	}
+
+
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {ping} from './api/ping';
-import {createUserApi, getAllUsersApi} from "./api/users";
-import {validateWithSchema} from "./api/validateschema"
+import { ping } from './api/ping';
+import { createUserApi, getAllUsersApi, loginUserApi } from "./api/users";
+import { validateWithSchema } from "./api/validateschema"
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-
 app.get('/', ping);
 
-const userRouter = express.Router();
+const router = express.Router();
 
+router.post('/users', createUserApi);
 
-userRouter.post('/users', validateWithSchema("login.schema.json"), createUserApi);
+router.get('/users', getAllUsersApi);
 
+router.post('/login', loginUserApi)
 
-userRouter.get('/users', getAllUsersApi);
-
-app.use('/api/v1', userRouter);
+app.use('/api/v1', router);
 
 export default app;
